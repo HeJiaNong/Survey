50e1f5564210e2d34fe8951a8114575be67a8643
何佳农 <jianonghe@gmail.com>
2018-07-18 17:27:46 +0800

    '本地js'

--- resources/views/admin/layouts/_editorShowMeta.blade.php
+++ resources/views/admin/layouts/_editorShowMeta.blade.php
@@ -1,10 +1,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-{{--<script src="{{ URL::asset('/static/admin/library/jquery.js') }}"></script>--}}
-{{--<script src="{{ URL::asset('/static/admin/library/survey.jquery.js') }}"></script>--}}
-{{--<link href="{{ URL::asset('/static/admin/library/survey.css') }}" type="text/css" rel="stylesheet"/>--}}
+<script src="{{ URL::asset('/static/admin/library/jquery.js') }}"></script>
+<script src="{{ URL::asset('/static/admin/library/survey.jquery.js') }}"></script>
+<link href="{{ URL::asset('/static/admin/library/survey.css') }}" type="text/css" rel="stylesheet"/>
 
 
-<script src="https://unpkg.com/jquery"></script>
-<script src="https://surveyjs.azureedge.net/1.0.33/survey.jquery.js"></script>
-<link href="https://surveyjs.azureedge.net/1.0.33/survey.css" type="text/css" rel="stylesheet"/>
+{{--<script src="https://unpkg.com/jquery"></script>--}}
+{{--<script src="https://surveyjs.azureedge.net/1.0.33/survey.jquery.js"></script>--}}
+{{--<link href="https://surveyjs.azureedge.net/1.0.33/survey.css" type="text/css" rel="stylesheet"/>--}}
